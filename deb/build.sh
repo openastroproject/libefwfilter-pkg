ae4301f73be91514073d3432b5f48a278e956780
James Fidell <james@openastroproject.org>
2018-10-31 22:48:02 +0000

    Skip use of -y flag for releases of dh_make that don't support it

--- deb/build.sh
+++ deb/build.sh
@@ -15,7 +15,13 @@ cd $srcdir
 tar zxf ../libefwfilter-$version.tar.gz
 chmod -x demo/c/*.*
 chmod -x demo/c/Makefile
-dh_make -y -l -f ../libefwfilter-$version.tar.gz
+YFLAG=-y
+dh_make -v | fgrep -q '1998-2011'
+if [ $? -eq 0 ]
+then
+  YFLAG=''
+fi
+dh_make $YFLAG -l -f ../libefwfilter-$version.tar.gz
 
 cp ../debfiles/control $debdir
 cp ../debfiles/copyright $debdir
